03ef2a14c153061985e110068ca8d72c9e0194a7
dosepehr <dosepehr@gmail.com>
2025-03-31 17:35:39 +0330

    add validating user password

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { comparePassword } from 'src/utils/funcs/password';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,13 @@ export class AuthService {
 
     async validateUser(name: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(name);
+        if (user) {
+            const isPasswordValid = await comparePassword(pass, user.password);
+            if (isPasswordValid) {
+                const { password, ...result } = user;
+                return result;
+            }
+        }
         return null;
     }
 }
