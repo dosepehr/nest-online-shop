e7e02ed36390c8d5bd8fe19b4c9230f2b3233836
dosepehr <dosepehr@gmail.com>
2025-03-31 17:02:31 +0330

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './resources/users/users.module';
 
 @Module({
   imports: [
@@ -11,10 +12,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: Number(process.env.DB_PORT),
       username: 'root',
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database: 'online_shop',
       entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
