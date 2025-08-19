ff5bd27b4a56b4cecffdaa01e4d6061fb6e82b58
dosepehr <dosepehr@gmail.com>
2025-08-19 16:47:57 +0330

    add env variables

--- config/db.config.ts
+++ config/db.config.ts
@@ -3,11 +3,11 @@ import { join } from 'path';
 
 export const dbConfig = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'online-shop',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [
     join(__dirname, '..', 'src', '**', 'entities', '*.entity.{ts,js}'),
   ],
