f0cd351f6dedcae9e97ac531a4e25c2bd69f96c2
dosepehr <dosepehr@gmail.com>
2025-09-04 10:29:06 +0330

    edit auth module

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,19 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AddressService } from './address.service';
+
 import { AdminAddressController } from './controllers/admin-address.controller';
 import { AddressController } from './controllers/address.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { User } from 'src/users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Address } from './entities/address.entity';
+import { AddressService } from './address.service';
+
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User, Address]),
-    JwtModule.register({
-      secret: 'yourSecretKey',
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([User, Address])],
   controllers: [AddressController, AdminAddressController],
   providers: [AddressService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
@@ -16,6 +16,6 @@ import { User } from 'src/users/entities/user.entity';
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
