ef928614622592cff46c375a75b1cc29ed84d17e
dosepehr <dosepehr@gmail.com>
2025-09-04 10:42:19 +0330

    add tickets resourse

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { dbConfig } from 'config/db.config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AddressModule } from './address/address.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AddressModule } from './address/address.module';
     UsersModule,
     AuthModule,
     AddressModule,
+    TicketModule,
   ],
   controllers: [AppController],
   providers: [AppService],
